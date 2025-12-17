42525efe76ab8f228406de6083069276d2f608e5
paulthanksgiving <paulthanksgiving2@gmail.com>
2025-12-17 02:56:45 +0100

    update11

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Logo from "../../assets/logo.png";
+import Logo from "../../assets/Logo.png";
 import { motion, AnimatePresence } from "framer-motion";
 import { Menu, X } from "lucide-react";
 
